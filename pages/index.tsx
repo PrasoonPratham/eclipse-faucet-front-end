a4812761f7be7f5b4950c205a5ff194d4050239c
Daniel <danm@workwithopal.com>
2023-01-27 12:23:11 -0800

    Network choice and wallet connect (#2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,83 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react'
+import Image from 'next/image'
+import { useState, useCallback, useMemo, useEffect, ReactNode } from 'react'
+import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { SolflareWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import dynamic from "next/dynamic";
+import { clusterApiUrl } from '@solana/web3.js';
+const ReactUIWalletModalProviderDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
+  { ssr: false }
+);
 
-const SELF_URL = process.env.NEXT_PUBLIC_SELF_URL
-const FAUCET_URL = process.env.NEXT_PUBLIC_ECLIPSE_FAUCET_URL
-const Home: NextPage = () => {
-  const [sending, setSending] = useState(false)
-  const [address, setAddress] = useState<string | null>(null)
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+type WalletProps = { children: ReactNode }
+
+export const Wallet = (props: WalletProps) => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter(),
+      new PhantomWalletAdapter()
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} >
+        <ReactUIWalletModalProviderDynamic>
+          <div className="wallet-connect">
+            <WalletMultiButton />
+            <span >Connect to fill-in your Solana address :-) </span>
+          </div>
+
+          {props.children}
+        </ReactUIWalletModalProviderDynamic>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export const FaucetForm = () => {
+
+  const [address, setAddress] = useState<string>("")
   const [amount, setAmount] = useState<string | null>(null)
+  const [sending, setSending] = useState(false)
+  const [eclipseRpcEndpoint, setEclipseRpcEndpoint] = useState("https://apricot.eclipsenetwork.xyz:8899")
   const [error, setError] = useState<string | null>(null)
   const [signature, setSignature] = useState<string | null>(null)
 
-  const onSend = async () => {
-    if (!FAUCET_URL || !address || !amount) return
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  
+  useEffect(() => {
+    if (publicKey !== null) {
+      setAddress(publicKey.toString());
+    }
+  }, [publicKey]);
+
+  const onSend = useCallback(async () => {
+    const faucet = `${eclipseRpcEndpoint}`
 
     setSending(true)
     setError(null)
-    const res = await fetch(FAUCET_URL, {
+    const res = await fetch(faucet, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -35,7 +96,43 @@ const Home: NextPage = () => {
     } else {
       setSignature(response.result)
     }
-  }
+  }, [address, amount, eclipseRpcEndpoint])
+
+  return (
+    <div className="form" >
+      <div className="form-label">Choose your Eclipse Network</div>
+      <input
+        value={eclipseRpcEndpoint}
+        onChange={(e) => setEclipseRpcEndpoint(e.target.value)}
+        placeholder="Eclipse Solana RPC endpoint"
+        type="text"
+      />
+      <div className="form-label">Wallet Address</div>
+      <input
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        placeholder="public key"
+        type="text"
+      />
+
+      <div className="form-label">Amount ($SOL)</div>
+      <input
+        style={{ width: "--webkit-fill-available" }}
+        value={amount ?? ''} onChange={(e) => setAmount(e.target.value)} placeholder="amount" type="number" min="0" step="0.001" />
+
+
+      <button className="send" type="submit" onClick={onSend}>
+        {sending ? 'Sending...' : 'Send'}
+      </button>
+      {signature && <p>Sent!</p>}
+      {error && <p>{error}</p>}
+    </div>
+  )
+
+}
+
+const SELF_URL = process.env.NEXT_PUBLIC_SELF_URL
+const Home: NextPage = () => {
 
   return (
     <div>
@@ -57,29 +154,19 @@ const Home: NextPage = () => {
         <meta property="og:image" content={`${SELF_URL}/eclipse_twitter_card.jpg`} />
       </Head>
       <div className="container">
-        <div className="title">
-          <div className="subhead">Eclipse</div>
-          <div className="header">Testnet Faucet</div>
+        <div className="icon">
+          <Image alt="Eclipse logo" src="/icon.svg" height={90} width={90} />
         </div>
 
-        <div className="form">
-          <div className="form-label">Wallet Address</div>
-          <input
-            value={address ?? ''}
-            onChange={(e) => setAddress(e.target.value)}
-            placeholder="public key"
-            type="text"
-          />
-
-          <div className="form-label">Amount ($MOON)</div>
-          <input value={amount ?? ''} onChange={(e) => setAmount(e.target.value)} placeholder="amount" type="number" min="0" step="0.001" />
-
-          <button className="send" type="submit" onClick={onSend}>
-            {sending ? 'Sending...' : 'Send'}
-          </button>
+        <div className="form-content">
+          <div className="title">
+            <div className="subhead">Eclipse</div>
+            <div className="header">Testnet Faucet</div>
+          </div>
+          <Wallet>
+            <FaucetForm />
+          </Wallet>
         </div>
-        {signature && <p>Sent!</p>}
-        {error && <p>{error}</p>}
       </div>
     </div>
   )
