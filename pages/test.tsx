c33ee78a13ef1fa860c5586dc65de74e7714d6c2
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-04-20 18:28:44 +0530

    Captcha added.

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,6 @@
 import { CheckIcon } from '@heroicons/react/20/solid'
 import { Transition } from '@headlessui/react'
+import ReCAPTCHA from 'react-google-recaptcha'
 import { useState } from 'react'
 
 const timeline = [
@@ -45,6 +46,15 @@ function classNames(...classes: string[]) {
 
 export default function Example() {
   const [visibleSections, setVisibleSections] = useState(1)
+  const [isCaptchaSolved, setIsCaptchaSolved] = useState(false)
+
+  const onCaptchaChange = (value: boolean) => {
+    if (value) {
+      setIsCaptchaSolved(true)
+    } else {
+      setIsCaptchaSolved(false)
+    }
+  }
 
   const handleButtonClick = (eventIdx: number) => {
     setVisibleSections(eventIdx + 2)
@@ -54,76 +64,82 @@ export default function Example() {
     setVisibleSections(1)
   }
 
-  return (
-    <div className="text-white p-12 rounded-lg shadow-lg bg-gray-500/25 md:mx-32 lg:mx-8 xl:mx-72">
-      <div className="flow-root">
-        <ul role="list" className="-mb-8">
-          {timeline.map((event, eventIdx) => (
-            <Transition
-              key={event.id}
-              show={eventIdx < visibleSections}
-              enter="transition-opacity duration-500"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity duration-500"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <li>
-                <div className="relative pb-8">
-                  {eventIdx !== timeline.length - 1 ? (
-                    <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-600" aria-hidden="true" />
-                  ) : null}
-                  <div className="relative flex space-x-3">
+return (
+  <div className="text-white p-12 rounded-lg shadow-lg bg-gray-500/25 md:mx-32 lg:mx-8 xl:mx-72">
+    <div className="flow-root">
+      <ul role="list" className="-mb-8">
+        {timeline.map((event, eventIdx) => (
+          <Transition
+            key={event.id}
+            show={eventIdx < visibleSections}
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <li>
+              <div className="relative pb-8">
+                {eventIdx !== timeline.length - 1 ? (
+                  <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-600" aria-hidden="true" />
+                ) : null}
+                <div className="relative flex space-x-3">
+                  <div>
+                    <span
+                      className={classNames(
+                        'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-gray-900',
+                        eventIdx < visibleSections ? 'bg-green-500' : 'bg-gray-500'
+                      )}
+                    >
+                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </div>
+                  <div className="flex flex-col min-w-0 flex-1 space-y-1.5">
                     <div>
-                      <span
-                        className={classNames(
-                          'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-gray-900',
-                          eventIdx < visibleSections ? 'bg-green-500' : 'bg-gray-500'
-                        )}
-                      >
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    </div>
-                    <div className="flex flex-col min-w-0 flex-1 space-y-1.5">
-                      <div>
-                        <p className="text-sm">
-                          {event.content}{' '}
-                          <a href={event.href} className="font-medium underline">
-                            {event.target}
-                          </a>
-                        </p>
-                      </div>
+                      <p className="text-sm">
+                        {event.content}{' '}
+                        <a href={event.href} className="font-medium underline">
+                          {event.target}
+                        </a>
+                      </p>
                     </div>
                   </div>
-                  {/* Modify the button for each section */}
-                  {eventIdx === visibleSections - 1 && eventIdx !== timeline.length - 1 && (
-                    <div className="flex justify-center">
-                      <button
-                        onClick={() => handleButtonClick(eventIdx)}
-                        className="inline-flex items-center my-3 px-3 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
-                      >
-                        {event.message}
-                      </button>
-                    </div>
-                  )}
                 </div>
-              </li>
-            </Transition>
-          ))}
-        </ul>
-        {visibleSections === timeline.length && (
-          <div className="flex justify-center -my-6">
-            <button
-              onClick={resetTimeline}
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
-            >
-              Start Again
-            </button>
-          </div>
-        )}
-      </div>
+                {eventIdx === 0 && (
+                  <div className="flex justify-center mt-2">
+                    <ReCAPTCHA sitekey="your-recaptcha-site-key" onChange={onCaptchaChange} />
+                  </div>
+                )}
+                {eventIdx === visibleSections - 1 && eventIdx !== timeline.length - 1 && (
+                  <div className="flex justify-center">
+                    <button
+                      onClick={() => handleButtonClick(eventIdx)}
+                      disabled={!isCaptchaSolved && eventIdx === 0}
+                      className="inline-flex items-center my-3 px-3 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
+                    >
+                      {event.message}
+                    </button>
+                  </div>
+                )}
+              </div>
+            </li>
+          </Transition>
+        ))}
+      </ul>
+      {visibleSections === timeline.length && (
+        <div className="flex justify-center -my-6">
+          <button
+            onClick={resetTimeline}
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
+          >
+            Start Again
+          </button>
+        </div>
+      )}
     </div>
-  )
+  </div>
+)
+
 
 }
