dc228a1d3c9595f017a8b322aa892839be09664d
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-04-20 18:17:56 +0530

    Button messages change dynamically.

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,31 +5,37 @@ import { useState } from 'react'
 const timeline = [
   {
     id: 1,
-    content: 'Complete Catcha:',
+    content: 'Complete Catcha.',
     target: 'Verify your identity',
     href: '#',
     message: 'Complete Captcha',
   },
   {
     id: 2,
-    content: 'Connect wallet:',
+    content: 'Connect wallet.',
     target: ' Connect to Metamask, backpack or any other Ethereum wallet',
     href: '#',
-    message: 'Connect your wallet to get started',
+    message: 'Connect your wallet',
   },
   {
     id: 3,
-    content: 'Connect Nautilus EVM:',
+    content: 'Connect Nautilus EVM.',
     target: 'Add the nautilus chain to your wallet',
     href: '#',
-    message: 'Connect Wallet',
+    message: 'Add Chain',
   },
   {
     id: 4,
-    content: 'Airdrop testnet tokens:',
+    content: 'Airdrop testnet tokens.',
     target: 'Receive testnet tokens to your wallet',
     href: '#',
     message: 'Airdrop',
+  },
+  {
+    id: 5,
+    content: 'Done!',
+    target: 'Your wallet has been funded with testnet tokens',
+    href: '#',
   },
 ]
 
@@ -53,46 +59,57 @@ export default function Example() {
       <div className="flow-root">
         <ul role="list" className="-mb-8">
           {timeline.map((event, eventIdx) => (
-            <li key={event.id}>
-              <div className="relative pb-8">
-                {eventIdx !== timeline.length - 1 ? (
-                  <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-600" aria-hidden="true" />
-                ) : null}
-                <div className="relative flex space-x-3">
-                  <div>
-                    <span
-                      className={classNames(
-                        'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-gray-900',
-                        eventIdx < visibleSections ? 'bg-green-500' : 'bg-gray-500'
-                      )}
-                    >
-                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                    </span>
-                  </div>
-                  <div className="flex flex-col min-w-0 flex-1 space-y-1.5">
+            <Transition
+              key={event.id}
+              show={eventIdx < visibleSections}
+              enter="transition-opacity duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <li>
+                <div className="relative pb-8">
+                  {eventIdx !== timeline.length - 1 ? (
+                    <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-600" aria-hidden="true" />
+                  ) : null}
+                  <div className="relative flex space-x-3">
                     <div>
-                      <p className="text-sm">
-                        {event.content}{' '}
-                        <a href={event.href} className="font-medium underline">
-                          {event.target}
-                        </a>
-                      </p>
+                      <span
+                        className={classNames(
+                          'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-gray-900',
+                          eventIdx < visibleSections ? 'bg-green-500' : 'bg-gray-500'
+                        )}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    </div>
+                    <div className="flex flex-col min-w-0 flex-1 space-y-1.5">
+                      <div>
+                        <p className="text-sm">
+                          {event.content}{' '}
+                          <a href={event.href} className="font-medium underline">
+                            {event.target}
+                          </a>
+                        </p>
+                      </div>
                     </div>
                   </div>
+                  {/* Modify the button for each section */}
+                  {eventIdx === visibleSections - 1 && eventIdx !== timeline.length - 1 && (
+                    <div className="flex justify-center">
+                      <button
+                        onClick={() => handleButtonClick(eventIdx)}
+                        className="inline-flex items-center my-3 px-3 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
+                      >
+                        {event.message}
+                      </button>
+                    </div>
+                  )}
                 </div>
-                {/* Modify the button for each section */}
-                {eventIdx === visibleSections - 1 && eventIdx !== timeline.length - 1 && (
-                  <div className="flex justify-center">
-                    <button
-                      onClick={() => handleButtonClick(eventIdx)}
-                      className="inline-flex items-center my-3 px-3 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4 mb-2"
-                    >
-                      Next Section
-                    </button>
-                  </div>
-                )}
-              </div>
-            </li>
+              </li>
+            </Transition>
           ))}
         </ul>
         {visibleSections === timeline.length && (
@@ -108,4 +125,5 @@ export default function Example() {
       </div>
     </div>
   )
+
 }
