d04fc7f678a7991c917324a2bd3f3694e1f59ae7
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-04-21 21:22:35 +0530

    Remove unused dependency

--- pages/test.tsx
+++ pages/test.tsx
@@ -7,7 +7,6 @@ import React from 'react';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 import { ConnectWalletButton } from './components/ConnectWalletButton';
-import { useWalletActive } from './components/ConnectWalletButton';
 
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
