e2e05bd60424b13ed41ef2ec949aec0252bfa013
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2023-05-02 14:46:21 +0530

    Fix dropdown switch button.

--- pages/components/AddNetworkButton.tsx
+++ pages/components/AddNetworkButton.tsx
@@ -217,7 +217,7 @@ export const AddNetworkButton: React.FC<AddNetworkButtonProps> = ({
         <>
           <select
             onChange={handleChainChange}
-            value={userSelectedChain?.chain_id || ""}
+            value={userSelectedChain?.chain_id || ''}
             className="block w-full bg-white border border-gray-200 text-gray-700 py-2 pl-3 pr-10 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-2 hover:border-gray-300 rounded transition duration-150 ease-in-out"
           >
             <option value="">Select a chain</option>
@@ -230,11 +230,12 @@ export const AddNetworkButton: React.FC<AddNetworkButtonProps> = ({
 
           <button
             onClick={handleAddNetworkClick}
-            disabled={!selectedChain}
-            className={`inline-flex items-center px-4 py-2 border-2 border-white focus:outline-none transition-all duration-300 ease-in ${!selectedChain
-                ? "bg-gray-300 text-gray-700 cursor-not-allowed"
-                : "bg-transparent text-white hover:bg-white hover:text-gray-700"
-              } focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+            disabled={!userSelectedChain}
+            className={`inline-flex items-center px-4 py-2 border-2 border-white focus:outline-none transition-all duration-300 ease-in ${
+              !selectedChain
+                ? 'bg-gray-300 text-gray-700 cursor-not-allowed'
+                : 'bg-transparent text-white hover:bg-white hover:text-gray-700'
+            } focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
           >
             {children}
           </button>
@@ -245,7 +246,7 @@ export const AddNetworkButton: React.FC<AddNetworkButtonProps> = ({
         </>
       )}
     </div>
-  );
+  )
 };
 
 export default AddNetworkButton;
